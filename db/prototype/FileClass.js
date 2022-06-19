c21f36fe7b6bddb67d0cf5ba38e469170e600229
dmejias08 <academicodiana08@gmail.com>
2022-06-18 21:55:39 -0600

    Chnage fileClass format

--- db/prototype/FileClass.js
+++ db/prototype/FileClass.js
@@ -3,91 +3,86 @@ const User = require("../models/user")
 const File = require("../models/file")
 
 
-class FileClass {
+const FileClass = {
 
-    findFileByName = async function findFileByName(fileName, route, owner) {
-        const file = await File.find({ "fileName": fileName, "route": route, "owner": owner })
-        console.log(file)
-        return file
-    }
-
-    findFileByTag = async function (tag, owner) {
-        const file = await File.find({ "tag": tag, "owner": owner })
-        console.log(file)
-        return file
-    }
-
-    createFile = async function (fileName, tag, route, owner, size) {
-        try {
-            const user = await User.find({ "username": owner })
-            console.log(user)
-            if (user.length != 0) {
-                const file = new File({
-                    fileName,
-                    tag,
-                    route,
-                    owner,
-                    size
-                })
-                const result = await file.save();
-                console.log("Se agregar lo siguiente:")
-                console.log(result);
-            } else {
-                console.log("No existe ese usuario")
+    createFile: (req, res) => {
+        const params = req.body
+        fileName = params.fileName
+        tag = params.tag
+        route = params.route
+        owner = params.owner
+        size = params.size
+        User.find({ "username": owner }, (err) => {
+            if (err) {
+                res.send("No existe el usuario")
             }
-        } catch {
-            console.log("Ya existe archivo")
+            const file = new File({
+                fileName,
+                tag,
+                route,
+                owner,
+                size
+            })
+            file.save((err, result) => {
+                if (err) {
+                    res.send("Archivo ya existe")
+                }
+                res.send(result)
+            })
         }
+        )
+    },
+
+    findFileByName: (req, res) => {
+        const params = req.body;
+        fileName = params.fileName
+        route = params.route
+        owner = params.owner
+        File.find({ "fileName": fileName, "route": route, "owner": owner }, (err, file) => {
+            if (err) {
+                res.send("No se encontro archivo")
+            }
+            res.send(file)
+        })
+    },
+
+    findFileByTag: (req, res) => {
+        const params = req.body
+        tag = params.tag
+        owner = params.owner
+        File.find({ "tag": tag, "owner": owner }, (err, file) => {
+            if (err) {
+                res.send("No se encontro archivo")
+            }
+            res.send(file)
+        })
+    },
+
+    listUsersFile: (req, res) => {
+        const params = req.body
+        owner = params.body
+        File.find({ "owner": owner }).select("owner fileName tag route").exec((err, result) => {
+            if (err) {
+                res.send("No se encontro archivo")
+            }
+            res.send(result)
+        })
+    },
+
+    addTagToFile: (req, res) => {
+        const params = req.body
+        fileName = params.fileName
+        tag = params.tag
+        route = params.route
+        owner = params.owner
+        const file = File.findOneAndUpdate({ 'fileName': fileName, "route": route, 'owner': owner }, { $set: { "tag": tag } })
+        file.save((err, result) => {
+            if (err) {
+                res.send("Ha ocurrido un error en la busqueda del archivo")
+            }
+            res.send(result)
+        })
     }
-
-    listFile = async function () {
-        const files = await File
-            .find()
-        console.log(files);
-    }
-
-    listUsersFile = async function (owner) {
-        const files = await File
-            .find({ "owner": owner })
-            .select("owner fileName tag route")
-        console.log(files);
-        return files
-    }
-
-    changeFileName = async function (fileName, newFileName, route, owner) {
-        try {
-            const file = await File.findOneAndUpdate({ 'fileName': fileName, "route": route, 'owner': owner }, {$set:{"fileName": newFileName}})
-            await file.save();
-        } catch (err) {
-            console.log("Ha ocurrido un error en la busqueda del archivo")
-        }
-
-    }
-
-    addTagToFile = async function (fileName, tag, route, owner) {
-        try {
-            const file = await File.findOneAndUpdate({ 'fileName': fileName, "route": route, 'owner': owner }, {$set:{"tag":tag}})
-            file.save();
-        } catch (err) {
-            console.log("Ha ocurrido un error en la busqueda del archivo")
-        }
-
-    }
-    
-
-    deleteFile = async function (fileName, route, owner) {
-        File.deleteOne({ "fileName": fileName, "route": route, "owner": owner }, function (err) {
-            if (err) return handleError(err);
-        });
-    }
-
-    deleteUserFiles = async function (owner) {
-        File.deleteMany({ "owner": owner }, function (err) {
-            if (err) return handleError(err);
-        });
-    }
-
-
 }
 
 
